caf33ef06213b3314632ada488d5d68c74e62c21
masterUNG <phrombutr@gmail.com>
2016-03-15 14:01:58 +0700

    Check Space Finish

--- app/src/main/java/krirk/punjawoot/snakesandladders/SignUpActivity.java
+++ app/src/main/java/krirk/punjawoot/snakesandladders/SignUpActivity.java
@@ -2,12 +2,66 @@ package krirk.punjawoot.snakesandladders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private RadioGroup avataRadioGroup;
+    private RadioButton avata1RadioButton, avata2RadioButton, avata3RadioButton,
+            avata4RadioButton, avata5RadioButton;
+    private String nameString, userString, passwordString, avataString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        bindWidget();
+
+
+    }   //  Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            Toast.makeText(SignUpActivity.this, "กรุณากรอกทุกช่อง คะ", Toast.LENGTH_SHORT).show();
+
+        } else {
+            //No Space
+
+        }   // if
+
+    }   // clickSignUpSign
+
+
+
+    private void bindWidget() {
+
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        avataRadioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+    }   // bindWidget
+
+}   // Main Class
